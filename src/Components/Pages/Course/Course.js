c92c0a17c29ab75c2ec0d8a0f2e728b0ac903339
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-10-26 02:28:04 +0600

    course page design done

--- src/Components/Pages/Course/Course.js
+++ src/Components/Pages/Course/Course.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Course = () => {
+  const courseTitle = useLoaderData();
+
+  console.log(courseTitle);
+
   return (
-    <div>
-      <h1>Course page</h1>
+    <div className="bg-gray-50 flex pb-4">
+      <div className="basis-1/5 px-2 mt-8">
+        <h1 className="text-2xl font-semibold">Course Title</h1>
+        <ul className="mt-4 list-inside list-decimal">
+          {courseTitle.map((title) => (
+            <li key={title.id} className="text-left  link link-hover">
+              <Link to={`/course/${title.id}`}>{title.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* right side */}
+      <div className="basis-4/5 mx-4">
+        <h1>course card</h1>
+        <div className="grid grid-cols-3 gap-6">
+          {courseTitle.map((title) => (
+            <div
+              key={title.id}
+              className="text-left bg-white rounded-md shadow border p-2"
+            >
+              <img
+                className="w-full border rounded-md"
+                src={title.img}
+                alt=""
+              />
+              <h1>{title.name}</h1>
+              <p>Name</p>
+              <Link to={`/course/${title.id}`}>
+                <button>Go</button>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../Components/Pages/Blog/Blog";
 import Course from "../Components/Pages/Course/Course";
+import CourseDetails from "../Components/Pages/CourseDetails/CourseDetails";
 import ErrorPage from "../Components/Pages/ErrorPage/ErrorPage";
 import FAQ from "../Components/Pages/FAQ/FAQ";
 import Home from "../Components/Pages/Home/Home";
@@ -15,7 +16,12 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       { path: "/", element: <Home></Home> },
-      { path: "/course", element: <Course></Course> },
+      {
+        path: "/course",
+        element: <Course></Course>,
+        loader: () => fetch("http://localhost:5000/course"),
+      },
+      { path: "/course/:courseID", element: <CourseDetails></CourseDetails> },
       { path: "/FAQ", element: <FAQ></FAQ> },
       { path: "login", element: <Login></Login> },
       { path: "register", element: <Register></Register> },
